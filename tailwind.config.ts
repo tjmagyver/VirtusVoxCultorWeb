b06fe2929705503d3dc7731547a32a6b209e08e1
carlosrobert0 <carlosroberto15755@gmail.com>
2023-11-28 12:13:56 -0300

    feat: adição de fontes e cores no theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,26 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        inriaSans: ['var(--font-inriaSans)']
       },
+      colors: {
+        white: '#FFFFFF',
+        black: '#000000',
+        red: {
+          500: '#ED3237',
+        },
+        blue: {
+          700: '#026180',
+        },
+        gray: {
+          100: '#D9D9D9',
+          300: '#767879',
+        }
+      }
     },
   },
   plugins: [],
